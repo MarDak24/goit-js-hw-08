9a0356ea982e0c91ba2d4ea675bf46ba2cab93c7
MarDak24 <maxim.matjerow@gmail.com>
2023-05-28 18:14:35 +0300

    first task - Done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const container = document.querySelector('.js-gallery');
 
-console.log('hello World')
+const markup = galleryItems.map(
+  ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link js-target" href="${original}">
+    <img
+      class="gallery__image js-target"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+);
+
+container.insertAdjacentHTML('beforeend', markup.join(''));
+
+const instance = new SimpleLightbox('.gallery__link');
+
+container.addEventListener('keydown', event => {
+  if (event.key === 'Escape' && instance.isOpen()) {
+    instance.close();
+  }
+});
