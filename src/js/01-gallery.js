f5d1d371cfe377899ce76d191e00c36e6a3c3d81
MarDak24 <maxim.matjerow@gmail.com>
2023-05-29 21:01:34 +0300

    final update HW-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,15 +6,15 @@ const container = document.querySelector('.js-gallery');
 
 const markup = galleryItems.map(
   ({ preview, original, description }) => `<li class="gallery__item">
-  <a class="gallery__link js-target" href="${original}">
-    <img
-      class="gallery__image js-target"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>`
+    <a class="gallery__link js-target" href="${original}">
+      <img
+        class="gallery__image js-target"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
 );
 
 container.insertAdjacentHTML('beforeend', markup.join(''));
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,27 @@
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-// Отримайте посилання на DOM-елемент iframe плеєра
-const playerIframe = document.getElementById('vimeo-player');
 
-// Отримайте поточний час відтворення з локального сховища
-const currentTimeFromStorage = localStorage.getItem('videoplayer-current-time');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-// Створіть новий плеєр з використанням iframe
-const player = new Vimeo.Player(playerIframe);
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
 
-// Якщо є збережений час відтворення, встановіть його на плеєрі
-if (currentTimeFromStorage) {
-  player.setCurrentTime(parseFloat(currentTimeFromStorage));
-}
+player.on('timeupdate', throttle(onPlay, 1000));
 
-// Оновлюйте час відтворення в локальному сховищі з інтервалом не частіше, ніж раз на секунду
-player.on(
-  'timeupdate',
-  throttle(function (event) {
-    const currentTime = event.seconds.toFixed(2);
-    localStorage.setItem('videoplayer-current-time', currentTime);
-  }, 1000)
-);
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    console.log('Плеєр перемістився на:', seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
